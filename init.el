3344522c10e228d745b418d26839c4ca3dfd3b4f
Kamin Horvath <khorvath@hokie.lan.kghorvath.com>
2023-07-21 12:18:05 -0400

    Initial commit

---
+++ init.el
@@ -0,0 +1,44 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+;; add MELPA package source
+
+(global-set-key (kbd "C-c l") #'org-store-link)
+(global-set-key (kbd "C-c a") #'org-agenda)
+(global-set-key (kbd "C-c c") #'org-capture)
+;; additional keybinds for org-mode
+
+(setq org-todo-keywords '((sequence "TODO" "IN-PROGRESS" "|" "DONE")))
+;; additional TODO keywords for org-mode
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(wdired-allow-to-change-permissions t)
+ '(custom-safe-themes
+   '("fee7287586b17efbfda432f05539b58e86e059e78006ce9237b8732fde991b4c" default))
+ '(org-agenda-files nil)
+ '(package-selected-packages
+   '(magit no-littering ement mastodon sudo-edit spacemacs-theme solarized-theme)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+(load-theme 'monokai t)
+;; load custom theme
+
+(scroll-bar-mode -1)
+;; customize appearance
+
+(require 'no-littering)
+(require 'sudo-edit)
+;; enable packages
+
+(setq mastodon-instance-url "https://social.kghorvath.com"
+      mastodon-active-user "kamin")
+;; mastodon
