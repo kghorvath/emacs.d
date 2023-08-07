e9ced1ec5aa64eae3ef5cc4492264a8ddbaddec0
Kamin Horvath <khorvath@hokie.lan.kghorvath.com>
2023-08-06 23:40:29 -0400

    Added conditional

--- init.el
+++ init.el
@@ -2,35 +2,8 @@
 (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/"))
 ;; add MELPA Stable package source
 
-(global-set-key (kbd "C-c l") #'org-store-link)
-(global-set-key (kbd "C-c a") #'org-agenda)
-(global-set-key (kbd "C-c c") #'org-capture)
-;; additional keybinds for org-mode
-
-(setq org-todo-keywords '((sequence "TODO" "IN-PROGRESS" "|" "DONE")))
-;; additional TODO keywords for org-mode
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("fee7287586b17efbfda432f05539b58e86e059e78006ce9237b8732fde991b4c" default))
- '(org-agenda-files nil)
- '(package-selected-packages
-   '(corfu elpy yaml-mode json-mode markdown-mode ef-themes auctex pdf-tools evil magit no-littering ement mastodon sudo-edit spacemacs-theme solarized-theme))
- '(wdired-allow-to-change-permissions t))
-;; Allow modifying permissions in wdired
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
 (scroll-bar-mode -1)
+(column-number-mode)
 ;; customize appearance
 
 (require 'no-littering)
@@ -44,10 +17,19 @@
 ;; enable modes
 
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
-(add-to-list 'load-path "~/.emacs.d/lisp/")
-(require 'init-mastodon)
-(require 'init-gnus)
-;; load additional files
+;; load additional themes
 
-(load-theme 'tokyo t)
+;;(when (display-graphic-p)
+  (load-theme 'wombat)
+;; (load-theme 'tokyo t)
 ;; load custom theme
+
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
+;; load custom file
+
+(load "~/.emacs.d/private/gnus.el")
+(load "~/.emacs.d/private/mastodon.el")
+(load "~/.emacs.d/lisp/keybindings.el")
+(load "~/.emacs.d/lisp/org-settings.el")
+;; load additional files
--- lisp/init-mastodon.el
+++
@@ -1,5 +0,0 @@
-(provide 'init-mastodon)
-
-(setq mastodon-instance-url "https://social.kghorvath.com"
-      mastodon-active-user "kamin")
-;; mastodon
