13c1be555a1af88e4047bf3177e8b72bf74fe82a
Kamin Horvath <khorvath@hokie.lan.kghorvath.com>
2023-08-07 12:04:22 -0400

    Moved custom-file to top

--- init.el
+++ init.el
@@ -2,6 +2,10 @@
 (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/"))
 ;; add MELPA Stable package source
 
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
+;; load custom file
+
 (scroll-bar-mode -1)
 (column-number-mode)
 ;; customize appearance
@@ -19,14 +23,10 @@
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
 ;; load additional themes
 
-;;(when (display-graphic-p)
-  (load-theme 'wombat)
+(when (display-graphic-p)
+  (load-theme 'wombat))
 ;; (load-theme 'tokyo t)
 ;; load custom theme
-
-(setq custom-file "~/.emacs.d/custom.el")
-(load custom-file)
-;; load custom file
 
 (load "~/.emacs.d/private/gnus.el")
 (load "~/.emacs.d/private/mastodon.el")
