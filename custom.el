39037cd5ab112e7626204ae387713e900ff70467
Kamin Horvath <khorvath@hokie.lan.kghorvath.com>
2023-08-06 23:32:59 -0400

    Reorganized into custom files.

---
+++ custom.el
@@ -0,0 +1,23 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("fee7287586b17efbfda432f05539b58e86e059e78006ce9237b8732fde991b4c" default))
+ '(delete-selection-mode nil)
+ '(dired-listing-switches "-alh")
+ '(display-time-mode t)
+ '(org-agenda-files nil)
+ '(package-selected-packages
+   '(org-bullets image-dired+ corfu elpy yaml-mode json-mode markdown-mode ef-themes auctex pdf-tools evil magit no-littering ement mastodon sudo-edit spacemacs-theme solarized-theme))
+ '(visible-bell t)
+ '(wdired-allow-to-change-permissions t))
+;; Allow modifying permissions in wdired
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Cascadia Mono PL" :foundry "SAJA" :slant normal :weight normal :height 113 :width normal)))))
---
+++ lisp/org-settings.el
@@ -0,0 +1,10 @@
+;; (add-hook 'org-mode-hook 'variable-pitch-mode)
+(setq org-hide-emphasis-markers t)
+;; (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+
+(font-lock-add-keywords 'org-mode
+                        '(("^ +\\([-*]\\) "
+                           (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+
+(setq org-todo-keywords '((sequence "TODO" "IN-PROGRESS" "|" "DONE")))
+;; additional TODO keywords
